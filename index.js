5e03a57df7fffcf938547f82244efeebdcf2a557
Craig <craig.coleman.cc@gmail.com>
2021-10-15 19:01:24 -0500

    commit tree ee58eba4fe60983ba9baa133d67dcf22fec34864

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function saturdayFun(activity) {
+    if (typeof activity === 'undefined') {
+        return 'This Saturday, I want to roller-skate!';
+    }else {
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+
+function mondayWork(activity) {
+    if (typeof activity === 'undefined') {
+        return 'This Monday, I will go to the office.';
+    } else {
+        return `This Monday, I will ${activity}.`
+    }
+}
+
+function wrapAdjective(parameter = "*"){
+    return function (adjective = "special") {
+        return `You are ${parameter}${adjective}${parameter}!`;
+    }
+}
